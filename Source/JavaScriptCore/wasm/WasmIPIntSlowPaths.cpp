e5391ad90f47f92e4d9cedf10a185caff74c5198
Shu-yu Guo <syg@apple.com>
2026-02-10 09:32:22 -0800

    [JSC] Do stack check on IPInt->BBQ loop OSR entry https://bugs.webkit.org/show_bug.cgi?id=307229 rdar://169866596
    Reviewed by Yusuke Suzuki.
    
    IPInt->BBQ loop reuses the IPInt frame, but BBQ frames may use more space.
    Currently we check for stack overflow and crash, instead of ahead of time. This
    PR changes the behavior to stay in IPInt if we don't have enough stack space.
    
    This check existed in the old WasmLLInt but wasn't ported over to IPInt.
    
    * Source/JavaScriptCore/wasm/WasmIPIntSlowPaths.cpp:
    (JSC::IPInt::WASM_IPINT_EXTERN_CPP_DECL):
    
    Canonical link: https://commits.webkit.org/307181@main

--- Source/JavaScriptCore/wasm/WasmIPIntSlowPaths.cpp
+++ Source/JavaScriptCore/wasm/WasmIPIntSlowPaths.cpp
@@ -303,6 +303,15 @@ WASM_IPINT_EXTERN_CPP_DECL(loop_osr, CallFrame* callFrame, uint8_t* pc, IPIntLoc
     auto* bbqCallee = uncheckedDowncast<Wasm::BBQCallee>(compiledCallee.get());
     ASSERT(bbqCallee->compilationMode() == Wasm::CompilationMode::BBQMode);
 
+    // The BBQ frame may use more stack than the IPInt frame. If there's not enough stack space,
+    // skip OSR and continue executing in IPInt.
+    if (bbqCallee->stackCheckSize() != Wasm::stackCheckNotNeeded) {
+        auto stackAtOSREntry = reinterpret_cast<uintptr_t>(pl - osrEntryData.numberOfStackValues);
+        auto candidateNewStackPointer = reinterpret_cast<void*>(stackAtOSREntry - bbqCallee->stackCheckSize());
+        if (candidateNewStackPointer < instance->softStackLimit()) [[unlikely]]
+            WASM_RETURN_TWO(nullptr, nullptr);
+    }
+
     auto* buffer = buildEntryBufferForLoopOSR(callee, bbqCallee, instance, osrEntryData, pl);
     if (!buffer)
         WASM_RETURN_TWO(nullptr, nullptr);
