7820f2e9b16a5e82d5ae4506cc7fda7a4c2f919c
Jonathan Bedard <jbedard@apple.com>
2026-02-14 21:17:52 -0800

    REGRESSION(307594@main): Use of undeclared identifier 'isTaintedScriptURLBlockable' https://bugs.webkit.org/show_bug.cgi?id=307930 rdar://170410351
    Unreviewed build fix.
    
    * Source/WebKit/Shared/ScriptTrackingPrivacyFilter.cpp:
    (WebKit::ScriptTrackingPrivacyFilter::shouldAllowAccess):
    (WebKit::ScriptTrackingPrivacyFilter::shouldBlockRequest):
    
    Canonical link: https://commits.webkit.org/307606@main

--- Source/WebKit/Shared/ScriptTrackingPrivacyFilter.cpp
+++ Source/WebKit/Shared/ScriptTrackingPrivacyFilter.cpp
@@ -96,7 +96,7 @@ bool ScriptTrackingPrivacyFilter::shouldAllowAccess(const URL& url, const WebCor
     if (url.isEmpty())
         return false;
 
-#if ENABLE(TRACKER_NETWORK_REQUEST_BLOCKING)
+#if ENABLE(TRACKER_NETWORK_REQUEST_BLOCKING) && ENABLE(ADVANCED_PRIVACY_PROTECTIONS)
     if (category == WebCore::ScriptTrackingPrivacyCategory::NetworkRequests && !isTaintedScriptURLBlockable(url))
         return true;
 #endif
@@ -113,7 +113,7 @@ bool ScriptTrackingPrivacyFilter::shouldBlockRequest(const URL& url, const WebCo
     if (url.isEmpty())
         return true;
 
-#if ENABLE(TRACKER_NETWORK_REQUEST_BLOCKING)
+#if ENABLE(TRACKER_NETWORK_REQUEST_BLOCKING) && ENABLE(ADVANCED_PRIVACY_PROTECTIONS)
     if (!isTaintedScriptURLBlockable(url))
         return false;
 #endif
