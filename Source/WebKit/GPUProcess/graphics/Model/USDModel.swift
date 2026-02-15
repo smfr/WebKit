cf3c63adf75d4098ab9506ff9357bf2eee416d92
Mike Wyrzykowski <mwyrzykowski@apple.com>
2026-02-14 22:31:59 -0800

    Disable linking to USD and Reality frameworks https://bugs.webkit.org/show_bug.cgi?id=307932 rdar://170412010
    Reviewed by Cameron McCormack.
    
    Disable linking to Reality and USD frameworks on macOS as it is
    causing a framework cycle.
    
    * Source/WebKit/GPUProcess/graphics/Model/ModelBridge.swift:
    * Source/WebKit/GPUProcess/graphics/Model/ModelIBLTextures.swift:
    * Source/WebKit/GPUProcess/graphics/Model/ModelParameters.swift:
    * Source/WebKit/GPUProcess/graphics/Model/ModelRenderer.swift:
    * Source/WebKit/GPUProcess/graphics/Model/ModelUtils.swift:
    * Source/WebKit/GPUProcess/graphics/Model/USDModel.swift:
    
    Canonical link: https://commits.webkit.org/307609@main

--- Source/WebKit/GPUProcess/graphics/Model/USDModel.swift
+++ Source/WebKit/GPUProcess/graphics/Model/USDModel.swift
@@ -26,7 +26,7 @@ internal import OSLog
 internal import WebKit_Internal
 internal import simd
 
-#if canImport(RealityCoreRenderer, _version: 9) && (os(macOS) || (os(iOS) && canImport(SwiftUI, _version: "8.0.36"))) && canImport(_USDKit_RealityKit)
+#if canImport(RealityCoreRenderer, _version: 9) && ((os(iOS) && canImport(SwiftUI, _version: "8.0.36"))) && canImport(_USDKit_RealityKit)
 @_weakLinked @_spi(UsdLoaderAPI) internal import _USDKit_RealityKit
 @_weakLinked @_spi(RealityCoreRendererAPI) internal import RealityKit
 @_weakLinked @_spi(RealityCoreTextureProcessingAPI) internal import RealityCoreTextureProcessing
