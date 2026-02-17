f7ba1ce943b10753000aa5f475f6e8be75f7e549
Abrar Rahman Protyasha <a_protyasha@apple.com>
2026-02-17 00:34:34 -0800

    Unnecessary UnvalidatedDigitalCredentialRequest.h include in WebPageProxy.h https://bugs.webkit.org/show_bug.cgi?id=308036 rdar://170529144
    Reviewed by Aditya Keerthi.
    
    The header contents are repeated verbatim later in WebPageProxy.h, so we
    are getting all the declarations for free anyway.
    
    * Source/WebKit/UIProcess/WebPageProxy.h:
    
    Canonical link: https://commits.webkit.org/307697@main

--- Source/WebKit/UIProcess/WebPageProxy.h
+++ Source/WebKit/UIProcess/WebPageProxy.h
@@ -29,7 +29,6 @@
 // Use forward declarations and WebPageProxyInternals.h instead.
 #include "APIObject.h"
 #include "MessageReceiver.h"
-#include <WebCore/UnvalidatedDigitalCredentialRequest.h>
 #include <wtf/ApproximateTime.h>
 #include <wtf/CheckedRef.h>
 #include <wtf/CompletionHandler.h>
