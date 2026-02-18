5a279c49d7d3d9a101b7a315729d9da7f5c2b441
Youenn Fablet <youenn@apple.com>
2026-02-18 11:56:20 -0800

    [macOS Debug]: ASSERTION FAILED: m_wrapper /Volumes/Data/worker/macOS-Tahoe-Debug-Build-EWS/build/Source/WebCore/bindings/js/JSEventListener.h(168) : JSC::JSObject *WebCore::JSEventListener::ensureJSFunction(ScriptExecutionContext &) const rdar://169044514 https://bugs.webkit.org/show_bug.cgi?id=306379
    Reviewed by Eric Carlson.
    
    As per https://w3c.github.io/webrtc-pc/#garbage-collection, a connection can be GCed when its closed slot is true,
    as no event should be fired on a closed connection.
    We update RTCPeerConnection::scheduleEvent lambda to exit early if the connection is closed.
    
    Canonical link: https://commits.webkit.org/307777@main

--- Source/WebCore/Modules/mediastream/RTCPeerConnection.cpp
+++ Source/WebCore/Modules/mediastream/RTCPeerConnection.cpp
@@ -995,6 +995,8 @@ void RTCPeerConnection::updateNegotiationNeededFlag(std::optional<uint32_t> even
 void RTCPeerConnection::scheduleEvent(Ref<Event>&& event)
 {
     queueTaskKeepingObjectAlive(*this, TaskSource::Networking, [event = WTF::move(event)](auto& connection) mutable {
+        if (connection.isClosed())
+            return;
         connection.dispatchEvent(event);
     });
 }
