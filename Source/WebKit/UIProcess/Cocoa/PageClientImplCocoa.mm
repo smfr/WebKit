a5d22c334dc61269c8815df77017d54c120869c2
Ryosuke Niwa <rniwa@webkit.org>
2026-02-19 02:27:33 -0800

    Fix new safer C++ warnings in PageClientImplCocoa.mm https://bugs.webkit.org/show_bug.cgi?id=308211
    Reviewed by Anne van Kesteren.
    
    Fix new warnings introduced by 307809@main.
    
    No new tests since there should be no behavior changes.
    
    * Source/WebKit/UIProcess/Cocoa/PageClientImplCocoa.mm:
    (WebKit::PageClientImplCocoa::didNotHandleTapAsClick):
    (WebKit::PageClientImplCocoa::didHandleTapAsHover):
    (WebKit::PageClientImplCocoa::didCompleteSyntheticClick):
    (WebKit::PageClientImplCocoa::commitPotentialTapFailed):
    (WebKit::PageClientImplCocoa::didGetTapHighlightGeometries):
    (WebKit::PageClientImplCocoa::isPotentialTapInProgress const):
    (WebKit::PageClientImplCocoa::disableDoubleTapGesturesDuringTapIfNecessary):
    (WebKit::PageClientImplCocoa::handleSmartMagnificationInformationForPotentialTap):
    
    Canonical link: https://commits.webkit.org/307822@main

--- Source/WebKit/UIProcess/Cocoa/PageClientImplCocoa.mm
+++ Source/WebKit/UIProcess/Cocoa/PageClientImplCocoa.mm
@@ -490,42 +490,42 @@ void PageClientImplCocoa::didCommitMainFrameData(const MainFrameData& mainFrameD
 
 void PageClientImplCocoa::didNotHandleTapAsClick(const WebCore::IntPoint& point)
 {
-    [m_webView _didNotHandleTapAsClick:point];
+    [webView() _didNotHandleTapAsClick:point];
 }
 
 void PageClientImplCocoa::didHandleTapAsHover()
 {
-    [m_webView _didHandleTapAsHover];
+    [webView() _didHandleTapAsHover];
 }
 
 void PageClientImplCocoa::didCompleteSyntheticClick()
 {
-    [m_webView _didCompleteSyntheticClick];
+    [webView() _didCompleteSyntheticClick];
 }
 
 void PageClientImplCocoa::commitPotentialTapFailed()
 {
-    [m_webView _commitPotentialTapFailed];
+    [webView() _commitPotentialTapFailed];
 }
 
 void PageClientImplCocoa::didGetTapHighlightGeometries(WebKit::TapIdentifier requestID, const WebCore::Color& color, const Vector<WebCore::FloatQuad>& highlightedQuads, const WebCore::IntSize& topLeftRadius, const WebCore::IntSize& topRightRadius, const WebCore::IntSize& bottomLeftRadius, const WebCore::IntSize& bottomRightRadius, bool nodeHasBuiltInClickHandling)
 {
-    [m_webView _didGetTapHighlightGeometries:requestID color:color quads:highlightedQuads topLeftRadius:topLeftRadius topRightRadius:topRightRadius bottomLeftRadius:bottomLeftRadius bottomRightRadius:bottomRightRadius nodeHasBuiltInClickHandling:nodeHasBuiltInClickHandling];
+    [webView() _didGetTapHighlightGeometries:requestID color:color quads:highlightedQuads topLeftRadius:topLeftRadius topRightRadius:topRightRadius bottomLeftRadius:bottomLeftRadius bottomRightRadius:bottomRightRadius nodeHasBuiltInClickHandling:nodeHasBuiltInClickHandling];
 }
 
 bool PageClientImplCocoa::isPotentialTapInProgress() const
 {
-    return [m_webView _isPotentialTapInProgress];
+    return [webView() _isPotentialTapInProgress];
 }
 
 void PageClientImplCocoa::disableDoubleTapGesturesDuringTapIfNecessary(WebKit::TapIdentifier requestID)
 {
-    [m_webView _disableDoubleTapGesturesDuringTapIfNecessary:requestID];
+    [webView() _disableDoubleTapGesturesDuringTapIfNecessary:requestID];
 }
 
 void PageClientImplCocoa::handleSmartMagnificationInformationForPotentialTap(WebKit::TapIdentifier requestID, const WebCore::FloatRect& renderRect, bool fitEntireRect, double viewportMinimumScale, double viewportMaximumScale, bool nodeIsRootLevel, bool nodeIsPluginElement)
 {
-    [m_webView _handleSmartMagnificationInformationForPotentialTap:requestID renderRect:renderRect fitEntireRect:fitEntireRect viewportMinimumScale:viewportMinimumScale viewportMaximumScale:viewportMaximumScale nodeIsRootLevel:nodeIsRootLevel nodeIsPluginElement:nodeIsPluginElement];
+    [webView() _handleSmartMagnificationInformationForPotentialTap:requestID renderRect:renderRect fitEntireRect:fitEntireRect viewportMinimumScale:viewportMinimumScale viewportMaximumScale:viewportMaximumScale nodeIsRootLevel:nodeIsRootLevel nodeIsPluginElement:nodeIsPluginElement];
 }
 
 #endif // ENABLE(TWO_PHASE_CLICKS)
