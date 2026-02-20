smfr/WebKit

commit fb08c78d67df648e6aef52eb2f8065c2495e1137

Plumb BorderShape through BorderPainter https://bugs.webkit.org/show_bug.cgi?id=288200 rdar://145296466
Reviewed by Antti Koivisto.

In preparation for supporting `corner-shape` which introduces non-rounded
corners, we need to reduce the amount of code in `BorderPainter` that assumes
that the border is a rect or rounded rect.

Do this by plumbing BorderShape in deeper; it can replace much of the data
that's currently stored in `BorderPainter::Sides`. Code is also simplified because
BorderShape internally does much of the rect vs. rounded-rect handling.

There's some complexity with `BleedAvoidance::BackgroundOverBorder` where we
shrink the inner border edge by a device pixel; previously this was accounted
for by `innerBorderAdjustment` when painting individual edges, but we can instead
just retrieve the original border widths from the BorderShape.

* LayoutTests/fast/borders/hidpi-outline-hairline-painting-expected.html:
* Source/WebCore/platform/graphics/Path.cpp:
(WebCore::Path::addPath):
* Source/WebCore/rendering/BorderEdge.cpp:
(WebCore::borderEdges):
* Source/WebCore/rendering/BorderEdge.h:
(WebCore::BorderEdge::width const):
(WebCore::BorderEdge::widthForPainting const):
(WebCore::borderEdges):
* Source/WebCore/rendering/BorderPainter.cpp:
(WebCore::BorderPainter::BorderPainter):
(WebCore::calculateSideRect):
(WebCore::sizeForDevicePixel):
(WebCore::shrinkRectByOneDevicePixel):
(WebCore::BorderPainter::paintBorder const):
(WebCore::BorderPainter::paintOutline const):
(WebCore::BorderPainter::paintSides const):
(WebCore::BorderPainter::paintTranslucentBorderSides const):
(WebCore::BorderPainter::paintBorderSides const):
(WebCore::BorderPainter::paintOneBorderSide const):
(WebCore::BorderPainter::clipBorderSidePolygon const):
(WebCore::BorderPainter::allCornersClippedOut): Deleted.
* Source/WebCore/rendering/BorderPainter.h:
* Source/WebCore/rendering/BorderShape.cpp:
(WebCore::BorderShape::shapeForOutlineRect):

Source/WebCore/rendering/BorderShape.cpp
@@ -102,14 +102,63 @@ BorderShape BorderShape::shapeForBorderRect(const RenderStyle& style, const Layo
     return BorderShape { borderRect, usedBorderWidths };
 }
 
+BorderShape BorderShape::shapeForOutlineRect(const RenderStyle& style, const LayoutRect& borderRect, const LayoutRect& outlineBoxRect, const RectEdges<LayoutUnit>& outlineWidths, RectEdges<bool> closedEdges)
+{
+    // top, right, bottom, left.
+    auto usedOutlineWidths = RectEdges<LayoutUnit> {
+        LayoutUnit(closedEdges.top() ? outlineWidths.top() : 0_lu),
+        LayoutUnit(closedEdges.right() ? outlineWidths.right() : 0_lu),
+        LayoutUnit(closedEdges.bottom() ? outlineWidths.bottom() : 0_lu),
+        LayoutUnit(closedEdges.left() ? outlineWidths.left() : 0_lu),
+    };
+
+    if (style.hasBorderRadius()) {
+        auto radii = calcRadiiFor(style.borderRadii(), borderRect.size());
+
+        auto leftOutset = std::max(borderRect.x() - outlineBoxRect.x(), 0_lu);
+        auto topOutset = std::max(borderRect.y() - outlineBoxRect.y(), 0_lu);
+        auto rightOutset = std::max(outlineBoxRect.maxX() - borderRect.maxX(), 0_lu);
+        auto bottomOutset = std::max(outlineBoxRect.maxY() - borderRect.maxY(), 0_lu);
+
+        radii.expand(topOutset, bottomOutset, leftOutset, rightOutset);
+
+        // FIXME: Share
+        if (!closedEdges.top()) {
+            radii.setTopLeft({ });
+            radii.setTopRight({ });
+        }
+        if (!closedEdges.right()) {
+            radii.setTopRight({ });
+            radii.setBottomRight({ });
+        }
+        if (!closedEdges.bottom()) {
+            radii.setBottomRight({ });
+            radii.setBottomLeft({ });
+        }
+        if (!closedEdges.left()) {
+            radii.setBottomLeft({ });
+            radii.setTopLeft({ });
+        }
+
+        if (!radii.areRenderableInRect(outlineBoxRect))
+            radii.makeRenderableInRect(outlineBoxRect);
+
+        return BorderShape { outlineBoxRect, usedOutlineWidths, radii };
+    }
+
+    return BorderShape { outlineBoxRect, usedOutlineWidths };
+}
+
 BorderShape::BorderShape(const LayoutRect& borderRect, const RectEdges<LayoutUnit>& borderWidths)
     : m_borderRect(borderRect)
+    , m_innerEdgeRect(computeInnerEdgeRoundedRect(m_borderRect, borderWidths))
     , m_borderWidths(borderWidths)
 {
 }
 
 BorderShape::BorderShape(const LayoutRect& borderRect, const RectEdges<LayoutUnit>& borderWidths, const RoundedRectRadii& radii)
     : m_borderRect(borderRect, radii)
+    , m_innerEdgeRect(computeInnerEdgeRoundedRect(m_borderRect, borderWidths))
     , m_borderWidths(borderWidths)
 {
     // The caller should have adjusted the radii already.
@@ -123,7 +172,7 @@ RoundedRect BorderShape::deprecatedRoundedRect() const
 
 RoundedRect BorderShape::deprecatedInnerRoundedRect() const
 {
-    return innerEdgeRoundedRect();
+    return m_innerEdgeRect;
 }
 
 FloatRoundedRect BorderShape::deprecatedPixelSnappedRoundedRect(float deviceScaleFactor) const
@@ -133,7 +182,7 @@ FloatRoundedRect BorderShape::deprecatedPixelSnappedRoundedRect(float deviceScal
 
 FloatRoundedRect BorderShape::deprecatedPixelSnappedInnerRoundedRect(float deviceScaleFactor) const
 {
-    return innerEdgeRoundedRect().pixelSnappedRoundedRectForPainting(deviceScaleFactor);
+    return m_innerEdgeRect.pixelSnappedRoundedRectForPainting(deviceScaleFactor);
 }
 
 FloatRect BorderShape::snappedOuterRect(float deviceScaleFactor) const
@@ -148,12 +197,22 @@ FloatRect BorderShape::snappedInnerRect(float deviceScaleFactor) const
 
 bool BorderShape::innerShapeContains(const LayoutRect& rect) const
 {
-    return innerEdgeRoundedRect().contains(rect);
+    return m_innerEdgeRect.contains(rect);
 }
 
 bool BorderShape::outerShapeContains(const LayoutRect& rect) const
 {
     return m_borderRect.contains(rect);
+}
+
+bool BorderShape::outerShapeIsRectangular() const
+{
+    return !m_borderRect.isRounded();
+}
+
+bool BorderShape::innerShapeIsRectangular() const
+{
+    return !m_innerEdgeRect.isRounded();
 }
 
 void BorderShape::move(LayoutSize offset)
@@ -184,7 +243,7 @@ Path BorderShape::pathForOuterShape(float deviceScaleFactor) const
 
 Path BorderShape::pathForInnerShape(float deviceScaleFactor) const
 {
-    auto pixelSnappedRect = innerEdgeRoundedRect().pixelSnappedRoundedRectForPainting(deviceScaleFactor);
+    auto pixelSnappedRect = m_innerEdgeRect.pixelSnappedRoundedRectForPainting(deviceScaleFactor);
     ASSERT(pixelSnappedRect.isRenderable());
 
     Path path;
@@ -192,10 +251,23 @@ Path BorderShape::pathForInnerShape(float deviceScaleFactor) const
     return path;
 }
 
+void BorderShape::addOuterShapeToPath(Path& path, float deviceScaleFactor) const
+{
+    auto pixelSnappedRect = m_borderRect.pixelSnappedRoundedRectForPainting(deviceScaleFactor);
+    addRoundedRectToPath(pixelSnappedRect, path);
+}
+
+void BorderShape::addInnerShapeToPath(Path& path, float deviceScaleFactor) const
+{
+    auto pixelSnappedRect = m_innerEdgeRect.pixelSnappedRoundedRectForPainting(deviceScaleFactor);
+    ASSERT(pixelSnappedRect.isRenderable());
+    addRoundedRectToPath(pixelSnappedRect, path);
+}
+
 Path BorderShape::pathForBorderArea(float deviceScaleFactor) const
 {
     auto pixelSnappedOuterRect = m_borderRect.pixelSnappedRoundedRectForPainting(deviceScaleFactor);
-    auto pixelSnappedInnerRect = innerEdgeRoundedRect().pixelSnappedRoundedRectForPainting(deviceScaleFactor);
+    auto pixelSnappedInnerRect = m_innerEdgeRect.pixelSnappedRoundedRectForPainting(deviceScaleFactor);
 
     ASSERT(pixelSnappedInnerRect.isRenderable());
 
@@ -205,7 +277,7 @@ Path BorderShape::pathForBorderArea(float deviceScaleFactor) const
     return path;
 }
 
-void BorderShape::clipToOuterShape(GraphicsContext& context, float deviceScaleFactor)
+void BorderShape::clipToOuterShape(GraphicsContext& context, float deviceScaleFactor) const
 {
     auto pixelSnappedRect = m_borderRect.pixelSnappedRoundedRectForPainting(deviceScaleFactor);
     if (pixelSnappedRect.isRounded())
@@ -214,9 +286,9 @@ void BorderShape::clipToOuterShape(GraphicsContext& context, float deviceScaleFa
         context.clip(pixelSnappedRect.rect());
 }
 
-void BorderShape::clipToInnerShape(GraphicsContext& context, float deviceScaleFactor)
+void BorderShape::clipToInnerShape(GraphicsContext& context, float deviceScaleFactor) const
 {
-    auto pixelSnappedRect = innerEdgeRoundedRect().pixelSnappedRoundedRectForPainting(deviceScaleFactor);
+    auto pixelSnappedRect = m_innerEdgeRect.pixelSnappedRoundedRectForPainting(deviceScaleFactor);
     ASSERT(pixelSnappedRect.isRenderable());
     if (pixelSnappedRect.isRounded())
         context.clipRoundedRect(pixelSnappedRect);
@@ -224,7 +296,7 @@ void BorderShape::clipToInnerShape(GraphicsContext& context, float deviceScaleFa
         context.clip(pixelSnappedRect.rect());
 }
 
-void BorderShape::clipOutOuterShape(GraphicsContext& context, float deviceScaleFactor)
+void BorderShape::clipOutOuterShape(GraphicsContext& context, float deviceScaleFactor) const
 {
     auto pixelSnappedRect = m_borderRect.pixelSnappedRoundedRectForPainting(deviceScaleFactor);
     if (pixelSnappedRect.isEmpty())
@@ -236,9 +308,9 @@ void BorderShape::clipOutOuterShape(GraphicsContext& context, float deviceScaleF
         context.clipOut(pixelSnappedRect.rect());
 }
 
-void BorderShape::clipOutInnerShape(GraphicsContext& context, float deviceScaleFactor)
+void BorderShape::clipOutInnerShape(GraphicsContext& context, float deviceScaleFactor) const
 {
-    auto pixelSnappedRect = innerEdgeRoundedRect().pixelSnappedRoundedRectForPainting(deviceScaleFactor);
+    auto pixelSnappedRect = m_innerEdgeRect.pixelSnappedRoundedRectForPainting(deviceScaleFactor);
     if (pixelSnappedRect.isEmpty())
         return;
 
@@ -248,7 +320,7 @@ void BorderShape::clipOutInnerShape(GraphicsContext& context, float deviceScaleF
         context.clipOut(pixelSnappedRect.rect());
 }
 
-void BorderShape::fillOuterShape(GraphicsContext& context, const Color& color, float deviceScaleFactor)
+void BorderShape::fillOuterShape(GraphicsContext& context, const Color& color, float deviceScaleFactor) const
 {
     auto pixelSnappedRect = m_borderRect.pixelSnappedRoundedRectForPainting(deviceScaleFactor);
     if (pixelSnappedRect.isRounded())
@@ -257,9 +329,9 @@ void BorderShape::fillOuterShape(GraphicsContext& context, const Color& color, f
         context.fillRect(pixelSnappedRect.rect(), color);
 }
 
-void BorderShape::fillInnerShape(GraphicsContext& context, const Color& color, float deviceScaleFactor)
+void BorderShape::fillInnerShape(GraphicsContext& context, const Color& color, float deviceScaleFactor) const
 {
-    auto pixelSnappedRect = innerEdgeRoundedRect().pixelSnappedRoundedRectForPainting(deviceScaleFactor);
+    auto pixelSnappedRect = m_innerEdgeRect.pixelSnappedRoundedRectForPainting(deviceScaleFactor);
     ASSERT(pixelSnappedRect.isRenderable());
     if (pixelSnappedRect.isRounded())
         context.fillRoundedRect(pixelSnappedRect, color);
@@ -267,32 +339,34 @@ void BorderShape::fillInnerShape(GraphicsContext& context, const Color& color, f
         context.fillRect(pixelSnappedRect.rect(), color);
 }
 
-RoundedRect BorderShape::innerEdgeRoundedRect() const
+RoundedRect BorderShape::computeInnerEdgeRoundedRect(const RoundedRect& borderRoundedRect, const RectEdges<LayoutUnit>& borderWidths)
 {
-    auto roundedRect = RoundedRect { innerEdgeRect() };
-    if (m_borderRect.isRounded()) {
-        auto innerRadii = m_borderRect.radii();
-        innerRadii.shrink(m_borderWidths.top(), m_borderWidths.bottom(), m_borderWidths.left(), m_borderWidths.right());
-        roundedRect.setRadii(innerRadii);
+    auto borderRect = borderRoundedRect.rect();
+    auto width = std::max(0_lu, borderRect.width() - borderWidths.left() - borderWidths.right());
+    auto height = std::max(0_lu, borderRect.height() - borderWidths.top() - borderWidths.bottom());
+    auto innerRect = LayoutRect {
+        borderRect.x() + borderWidths.left(),
+        borderRect.y() + borderWidths.top(),
+        width,
+        height
+    };
+
+    auto innerEdgeRect = RoundedRect { innerRect };
+    if (borderRoundedRect.isRounded()) {
+        auto innerRadii = borderRoundedRect.radii();
+        innerRadii.shrink(borderWidths.top(), borderWidths.bottom(), borderWidths.left(), borderWidths.right());
+        innerEdgeRect.setRadii(innerRadii);
+
+        if (!innerEdgeRect.isRenderable())
+            innerEdgeRect.adjustRadii();
     }
 
-    if (!roundedRect.isRenderable())
-        roundedRect.adjustRadii();
-
-    return roundedRect;
+    return innerEdgeRect;
 }
 
 LayoutRect BorderShape::innerEdgeRect() const
 {
-    auto& borderRect = m_borderRect.rect();
-    auto width = std::max(0_lu, borderRect.width() - m_borderWidths.left() - m_borderWidths.right());
-    auto height = std::max(0_lu, borderRect.height() - m_borderWidths.top() - m_borderWidths.bottom());
-    return {
-        borderRect.x() + m_borderWidths.left(),
-        borderRect.y() + m_borderWidths.top(),
-        width,
-        height
-    };
+    return m_innerEdgeRect.rect();
 }
 
 } // namespace WebCore

Source/WebCore/rendering/BorderShape.h
@@ -51,16 +51,17 @@ public:
     // overrideBorderWidths describe custom insets from the border box, used instead of the border widths from the style.
     static BorderShape shapeForBorderRect(const RenderStyle&, const LayoutRect& borderRect, const RectEdges<LayoutUnit>& overrideBorderWidths, RectEdges<bool> closedEdges = { true });
 
+    // Create a BorderShape suitable for rendering an outline. borderRect is provided to allow for scaling the corner radii.
+    static BorderShape shapeForOutlineRect(const RenderStyle&, const LayoutRect& borderRect, const LayoutRect& outlineBoxRect, const RectEdges<LayoutUnit>& outlineWidths, RectEdges<bool> closedEdges = { true });
+
     BorderShape(const LayoutRect& borderRect, const RectEdges<LayoutUnit>& borderWidths);
     BorderShape(const LayoutRect& borderRect, const RectEdges<LayoutUnit>& borderWidths, const RoundedRectRadii&);
 
-    BorderShape(const BorderShape& other)
-        : m_borderRect(other.m_borderRect)
-        , m_borderWidths(other.m_borderWidths)
-    {
-    }
+    BorderShape(const BorderShape&) = default;
 
     LayoutRect borderRect() const { return m_borderRect.rect(); }
+    // Takes `closedEdges` into account.
+    const RectEdges<LayoutUnit>& borderWidths() const { return m_borderWidths; }
 
     RoundedRect deprecatedRoundedRect() const;
     RoundedRect deprecatedInnerRoundedRect() const;
@@ -78,6 +79,10 @@ public:
     FloatRect snappedInnerRect(float deviceScaleFactor) const;
 
     bool isRounded() const { return m_borderRect.isRounded(); }
+
+    bool outerShapeIsRectangular() const;
+    bool innerShapeIsRectangular() const;
+
     bool isEmpty() const { return m_borderRect.rect().isEmpty(); }
 
     void move(LayoutSize);
@@ -88,22 +93,26 @@ public:
     Path pathForOuterShape(float deviceScaleFactor) const;
     Path pathForInnerShape(float deviceScaleFactor) const;
 
+    void addOuterShapeToPath(Path&, float deviceScaleFactor) const;
+    void addInnerShapeToPath(Path&, float deviceScaleFactor) const;
+
     Path pathForBorderArea(float deviceScaleFactor) const;
 
-    void clipToOuterShape(GraphicsContext&, float deviceScaleFactor);
-    void clipToInnerShape(GraphicsContext&, float deviceScaleFactor);
+    void clipToOuterShape(GraphicsContext&, float deviceScaleFactor) const;
+    void clipToInnerShape(GraphicsContext&, float deviceScaleFactor) const;
 
-    void clipOutOuterShape(GraphicsContext&, float deviceScaleFactor);
-    void clipOutInnerShape(GraphicsContext&, float deviceScaleFactor);
+    void clipOutOuterShape(GraphicsContext&, float deviceScaleFactor) const;
+    void clipOutInnerShape(GraphicsContext&, float deviceScaleFactor) const;
 
-    void fillOuterShape(GraphicsContext&, const Color&, float deviceScaleFactor);
-    void fillInnerShape(GraphicsContext&, const Color&, float deviceScaleFactor);
+    void fillOuterShape(GraphicsContext&, const Color&, float deviceScaleFactor) const;
+    void fillInnerShape(GraphicsContext&, const Color&, float deviceScaleFactor) const;
 
 private:
-    RoundedRect innerEdgeRoundedRect() const;
     LayoutRect innerEdgeRect() const;
+    static RoundedRect computeInnerEdgeRoundedRect(const RoundedRect& borderRoundedRect, const RectEdges<LayoutUnit>& borderWidths);
 
     RoundedRect m_borderRect;
+    RoundedRect m_innerEdgeRect;
     RectEdges<LayoutUnit> m_borderWidths;
 };
 

LayoutTests/fast/borders/hidpi-outline-hairline-painting-expected.html
@@ -14,22 +14,22 @@
 <body>
 <div style="border-style: solid; top: 9.5px; left: 9.5px;"></div>
 <div style="border-style: solid; top: 9.5px; left: 24.5px;"></div>
-<div style="border-style: solid; top: 9.5px; left: 39.5px;"></div>
-<div style="border-style: solid; top: 9.5px; left: 54.5px;"></div>
+<div style="border-style: solid; width: 6px; height: 6px; top: 9px; left: 39px;"></div>
+<div style="border-style: solid; width: 6px; height: 6px; top: 9px; left: 54px;"></div>
 <div style="border-style: solid; width: 6px; height: 6px; top: 9px; left: 69px;"></div>
 <div style="border-style: solid; width: 6px; height: 6px; top: 9px; left: 84px;"></div>
 <div style="border-style: solid; width: 6px; height: 6px; top: 9px; left: 99px;"></div>
-<div style="border-style: solid; width: 6px; height: 6px; top: 9px; left: 114px;"></div>
-<div style="border-style: solid; width: 6px; height: 6px; top: 9px; left: 129px;"></div>
+<div style="border-style: solid; width: 7px; height: 7px; top: 8.5px; left: 113.5px;"></div>
+<div style="border-style: solid; width: 7px; height: 7px; top: 8.5px; left: 128.5px;"></div>
 <div style="border-style: solid; border-width: 1px; width: 7px; height: 7px; top: 8px; left: 143px;"></div>
 <div style="border-style: solid; border-width: 1px; width: 7px; height: 7px; top: 8px; left: 158px;"></div>
 
-<div style="border-style: double; top: 24px; left: 9px; width: 6px; height: 6px;"></div>
-<div style="border-style: double; top: 24px; left: 24px; width: 6px; height: 6px;"></div>
+<div style="border-style: double; top: 23.5px; left: 8.5px; width: 7px; height: 7px;"></div>
+<div style="border-style: double; top: 23.5px; left: 23.5px; width: 7px; height: 7px;"></div>
 <div style="border-style: double; border-width: 1px; top: 23px; left: 38px; width: 7px; height: 7px;"></div>
 <div style="border-style: double; border-width: 1px; top: 23px; left: 53px; width: 7px; height: 7px;"></div>
 <div style="border-style: double; border-width: 1px; top: 23px; left: 68px; width: 7px; height: 7px;"></div>
-<div style="border-style: double; border-width: 1px; top: 23px; left: 83px; width: 7px; height: 7px;"></div>
-<div style="border-style: double; border-width: 1px; top: 23px; left: 98px; width: 7px; height: 7px;"></div>
+<div style="border-style: double; border-width: 1px; top: 22.5px; left: 82.5px; width: 8px; height: 8px;"></div>
+<div style="border-style: double; border-width: 1px; top: 22.5px; left: 97.5px; width: 8px; height: 8px;"></div>
 </body>
 </html>

Source/WebCore/platform/graphics/Path.cpp
@@ -333,6 +333,7 @@ void Path::addPath(const Path& path, const AffineTransform& transform)
     if (path.isEmpty() || !transform.isInvertible())
         return;
 
+    // FIXME: This should inspect the incoming path and add just the segments if possible.
     ensurePlatformPathImpl().addPath(const_cast<Path&>(path).ensurePlatformPathImpl(), transform);
 }
 

Source/WebCore/rendering/BorderEdge.cpp
@@ -46,18 +46,19 @@ BorderEdge::BorderEdge(float edgeWidth, Color edgeColor, BorderStyle edgeStyle, 
     m_flooredToDevicePixelWidth = floorf(edgeWidth * devicePixelRatio) / devicePixelRatio;
 }
 
-BorderEdges borderEdges(const RenderStyle& style, float deviceScaleFactor, RectEdges<bool> closedEdges, bool setColorsToBlack)
+BorderEdges borderEdges(const RenderStyle& style, float deviceScaleFactor, RectEdges<bool> closedEdges, LayoutSize inflation, bool setColorsToBlack)
 {
-    auto constructBorderEdge = [&](float width, CSSPropertyID borderColorProperty, BorderStyle borderStyle, bool isTransparent, bool isPresent) {
+    auto constructBorderEdge = [&](float width, float inflation, CSSPropertyID borderColorProperty, BorderStyle borderStyle, bool isTransparent, bool isPresent) {
         auto color = setColorsToBlack ? Color::black : style.visitedDependentColorWithColorFilter(borderColorProperty);
-        return BorderEdge(width, color, borderStyle, !setColorsToBlack && isTransparent, isPresent, deviceScaleFactor);
+        auto inflatedWidth = width ? width + inflation : width;
+        return BorderEdge(inflatedWidth, color, borderStyle, !setColorsToBlack && isTransparent, isPresent, deviceScaleFactor);
     };
 
     return {
-        constructBorderEdge(style.borderTopWidth(), CSSPropertyBorderTopColor, style.borderTopStyle(), style.borderTopIsTransparent(), closedEdges.top()),
-        constructBorderEdge(style.borderRightWidth(), CSSPropertyBorderRightColor, style.borderRightStyle(), style.borderRightIsTransparent(), closedEdges.right()),
-        constructBorderEdge(style.borderBottomWidth(), CSSPropertyBorderBottomColor, style.borderBottomStyle(), style.borderBottomIsTransparent(), closedEdges.bottom()),
-        constructBorderEdge(style.borderLeftWidth(), CSSPropertyBorderLeftColor, style.borderLeftStyle(), style.borderLeftIsTransparent(), closedEdges.left())
+        constructBorderEdge(style.borderTopWidth(), inflation.height().toFloat(), CSSPropertyBorderTopColor, style.borderTopStyle(), style.borderTopIsTransparent(), closedEdges.top()),
+        constructBorderEdge(style.borderRightWidth(), inflation.width().toFloat(), CSSPropertyBorderRightColor, style.borderRightStyle(), style.borderRightIsTransparent(), closedEdges.right()),
+        constructBorderEdge(style.borderBottomWidth(), inflation.height().toFloat(), CSSPropertyBorderBottomColor, style.borderBottomStyle(), style.borderBottomIsTransparent(), closedEdges.bottom()),
+        constructBorderEdge(style.borderLeftWidth(), inflation.width().toFloat(), CSSPropertyBorderLeftColor, style.borderLeftStyle(), style.borderLeftIsTransparent(), closedEdges.left())
     };
 }
 

commit 54b6555c4fa661e33fb980e8d541ab22a608b2e5

Rotating bounding box invalidation does not redraw a border on other element rdar://170438404 https://bugs.webkit.org/show_bug.cgi?id=307949
Reviewed by Simon Fraser.

The regression from 290951@main replaced allCornersClippedOut() with
outerShapeContains(), but these have different semantics. outerShapeContains
checks if the dirty rect is inside the rounded shape, while
allCornersClippedOut() checked if the dirty rect avoids all corner regions.
A rect inside the shape but within a corner region still needs curved-path
painting. Re-add allCornersClippedOut() to restore the correct
operation for this case.

Test: fast/repaint/border-radius-partial-repaint-corner.html

* LayoutTests/fast/repaint/border-radius-partial-repaint-corner-expected.html: Added.
* LayoutTests/fast/repaint/border-radius-partial-repaint-corner.html: Added.
* Source/WebCore/rendering/BorderPainter.cpp:
(WebCore::BorderPainter::paintBorder const):
* Source/WebCore/rendering/BorderShape.cpp:
(WebCore::BorderShape::allCornersClippedOut const):
* Source/WebCore/rendering/BorderShape.h:

Canonical link: https://commits.webkit.org/307930@main

Source/WebCore/rendering/BorderPainter.cpp
@@ -270,13 +270,14 @@ void BorderPainter::paintBorder(const LayoutRect& rect, const RenderStyle& style
         return std::tuple<BorderShape, BorderEdges> { BorderShape({ }, { 0_lu }), { } };
     }();
 
-    bool outerEdgeIsRectangular = !shape.isRounded() || shape.outerShapeContains(m_paintInfo.rect);
+    bool haveAllSolidEdges = decorationHasAllSolidEdges(edges);
+    bool outerEdgeIsRectangular = !shape.isRounded() || (haveAllSolidEdges && shape.allCornersClippedOut(m_paintInfo.rect));
     bool innerEdgeIsRectangular = shape.innerShapeIsRectangular();
 
     paintSides(shape, {
         style.border().hasBorderRadius() ? std::optional { style.borderRadii() } : std::nullopt,
         edges,
-        decorationHasAllSolidEdges(edges),
+        haveAllSolidEdges,
         outerEdgeIsRectangular,
         innerEdgeIsRectangular,
         bleedAvoidance,

Source/WebCore/rendering/BorderShape.cpp
@@ -218,6 +218,40 @@ bool BorderShape::outerShapeContains(const LayoutRect& rect) const
     return m_borderRect.contains(rect);
 }
 
+bool BorderShape::allCornersClippedOut(const LayoutRect& rect) const
+{
+    if (!isRounded())
+        return true;
+
+    auto borderRect = m_borderRect.rect();
+    if (rect.contains(borderRect))
+        return false;
+
+    auto radii = m_borderRect.radii();
+
+    LayoutRect topLeftRect(borderRect.location(), radii.topLeft());
+    if (rect.intersects(topLeftRect))
+        return false;
+
+    LayoutRect topRightRect(borderRect.location(), radii.topRight());
+    topRightRect.setX(borderRect.maxX() - topRightRect.width());
+    if (rect.intersects(topRightRect))
+        return false;
+
+    LayoutRect bottomLeftRect(borderRect.location(), radii.bottomLeft());
+    bottomLeftRect.setY(borderRect.maxY() - bottomLeftRect.height());
+    if (rect.intersects(bottomLeftRect))
+        return false;
+
+    LayoutRect bottomRightRect(borderRect.location(), radii.bottomRight());
+    bottomRightRect.setX(borderRect.maxX() - bottomRightRect.width());
+    bottomRightRect.setY(borderRect.maxY() - bottomRightRect.height());
+    if (rect.intersects(bottomRightRect))
+        return false;
+
+    return true;
+}
+
 bool BorderShape::outerShapeIsRectangular() const
 {
     return !m_borderRect.isRounded();

Source/WebCore/rendering/BorderShape.h
@@ -75,9 +75,13 @@ public:
     FloatRoundedRect deprecatedPixelSnappedRoundedRect(float deviceScaleFactor) const;
     FloatRoundedRect deprecatedPixelSnappedInnerRoundedRect(float deviceScaleFactor) const;
 
-    // Returns true if the given rect is entirely inside the shape, without impinging on any of the corners.
+    // Returns true if the given rect is entirely inside the inner/outer shape.
     bool innerShapeContains(const LayoutRect&) const;
     bool outerShapeContains(const LayoutRect&) const;
+
+    // Returns true if no corner regions of the outer border intersect the given rect,
+    // meaning border painting can use simpler rectangular paths.
+    bool allCornersClippedOut(const LayoutRect&) const;
 
     const LayoutRoundedRectRadii& radii() const { return m_borderRect.radii(); }
     void setRadii(const LayoutRoundedRectRadii& radii) { m_borderRect.setRadii(radii); }

LayoutTests/fast/repaint/border-radius-partial-repaint-corner-expected.html
@@ -1,0 +1,24 @@
+<!DOCTYPE html>
+<style>
+body { margin: 0; }
+#trigger {
+    position: absolute;
+    top: 65px;
+    left: 65px;
+    width: 20px;
+    height: 20px;
+    background: lightgray;
+}
+#bordered {
+    position: absolute;
+    top: 50px;
+    left: 50px;
+    width: 200px;
+    height: 200px;
+    border: 20px solid black;
+    border-radius: 40px;
+    background: white;
+}
+</style>
+<div id="trigger"></div>
+<div id="bordered"></div>

LayoutTests/fast/repaint/border-radius-partial-repaint-corner.html
@@ -1,0 +1,43 @@
+<!DOCTYPE html>
+<script src="../../resources/ui-helper.js"></script>
+<style>
+body { margin: 0; }
+#trigger {
+    position: absolute;
+    top: 65px;
+    left: 65px;
+    width: 20px;
+    height: 20px;
+    background: white;
+}
+#bordered {
+    position: absolute;
+    top: 50px;
+    left: 50px;
+    width: 200px;
+    height: 200px;
+    border: 20px solid black;
+    border-radius: 40px;
+    background: white;
+}
+</style>
+<div id="trigger"></div>
+<div id="bordered"></div>
+<script>
+if (window.testRunner) {
+    testRunner.waitUntilDone();
+    testRunner.dontForceRepaint();
+}
+
+window.addEventListener('load', async () => {
+    await UIHelper.renderingUpdate();
+
+    document.getElementById('trigger').style.backgroundColor = 'lightgray';
+
+    await UIHelper.renderingUpdate();
+    await UIHelper.renderingUpdate();
+
+    if (window.testRunner)
+        testRunner.notifyDone();
+});
+</script>