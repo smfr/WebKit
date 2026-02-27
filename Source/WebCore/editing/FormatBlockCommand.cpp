51dde913fc8d73958d25795795d6507e002b67bc
j0pan <jpan0917@gmail.com>
2026-02-27 13:56:43 -0800

    execCommand('FormatBlock') does not preserve inline styles of the replaced block elements https://bugs.webkit.org/show_bug.cgi?id=47054 rdar://157657531
    Reviewed by Ryosuke Niwa.
    
    When execCommand("formatBlock") converts one block element to another
    (e.g. <p> to <div>), copy the style attribute from the original element
    to the new one so inline styles like color and font-size are preserved.
    
    Test: editing/execCommand/format-block-preserves-inline-style.html
    
    * LayoutTests/editing/execCommand/format-block-preserves-inline-style-expected.txt: Added.
    * LayoutTests/editing/execCommand/format-block-preserves-inline-style.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/editing/run/formatblock_4001-5000-expected.txt:
    * Source/WebCore/editing/FormatBlockCommand.cpp:
    (WebCore::FormatBlockCommand::formatRange):
    
    Canonical link: https://commits.webkit.org/308365@main

--- Source/WebCore/editing/FormatBlockCommand.cpp
+++ Source/WebCore/editing/FormatBlockCommand.cpp
@@ -26,11 +26,14 @@
 #include "config.h"
 #include "FormatBlockCommand.h"
 
+#include "CSSSerializationContext.h"
 #include "Document.h"
 #include "Editing.h"
+#include "EditingStyle.h"
 #include "Element.h"
 #include "HTMLElement.h"
 #include "HTMLNames.h"
+#include "MutableStyleProperties.h"
 #include "NodeName.h"
 #include "VisibleUnits.h"
 #include <wtf/NeverDestroyed.h>
@@ -93,6 +96,18 @@ void FormatBlockCommand::formatRange(const Position& start, const Position& end,
         // this by splitting all parents of the current paragraph up to that point.
         blockNode = createBlockElement();
         insertNodeBefore(*blockNode, *nodeAfterInsertionPosition);
+        // Preserve the inline style of the element being replaced so that formatting
+        // such as color and font properties are not lost when the block type changes.
+        // See: https://bugs.webkit.org/show_bug.cgi?id=47054
+        if (RefPtr element = dynamicDowncast<HTMLElement>(nodeAfterInsertionPosition)) {
+            if (element->hasAttribute(styleAttr)) {
+                auto style = EditingStyle::create(element->inlineStyle());
+                if (RefPtr styledBlockNode = dynamicDowncast<StyledElement>(blockNode))
+                    style->removeStyleFromRulesAndContext(*styledBlockNode, styledBlockNode->parentNode());
+                if (!style->isEmpty())
+                    blockNode->setAttribute(styleAttr, AtomString { style->style()->asText(CSS::defaultSerializationContext()) });
+            }
+        }
     }
 
     RefPtr lastChild = blockNode->lastChild();
